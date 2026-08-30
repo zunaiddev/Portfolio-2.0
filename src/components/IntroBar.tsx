1006262f0c80367db1b11af7579620ae2d0d872b
zunaiddev <ja7667924@gmail.com>
2026-08-31 01:02:50 +0530

    Created Landing page

--- src/components/IntroBar.tsx
+++ src/components/IntroBar.tsx
@@ -1,12 +1,27 @@
 import type {JSX} from "react";
-import {NAME, ROLE} from "../constants/constants.ts";
+import {NAME} from "../constants/constants.ts";
 
 
 function IntroBar(): JSX.Element {
     return (<div
-        className="z-1 mt-24 mb-8 flex flex-col items-center text-center md:mt-4 md:mb-12 opacity-100 transform-none">
-        <h2 className="text-secondary-foreground mt-1 text-xl font-semibold md:text-2xl">Hey, I'm {NAME}</h2>
-        <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">{ROLE}</h1>
+        className="z-1 px-6 flex w-full pointer-events-none items-center justify-between text-center">
+        <div className="text-start">
+            <h2 className="text-accent font-geist mt-1 text-[25px]">Hello! I'm</h2>
+            <h1 className="font-geist text-soft font-medium -mt-4 text-[40px]">{NAME}</h1>
+        </div>
+
+        <div className="text-start">
+            <h2 className="text-accent font-geist mt-1 text-[25px]">An</h2>
+            <div>
+                <h2 className="text-accent uppercase font-geist relative font-semibold text-[45px] tracking-[2px] spacing after:content-[''] after:absolute after:top-1 after:left-0 after:z-3 after:w-full after:h-[90%] after:bg-[linear-gradient(0deg,var(--backgroundColor)_40%,rgba(0,0,0,0)_110%)] leading-9">
+                    Software Engineer
+                </h2>
+                <h2 className="text-soft ml-5 font-geist relative font-semibold text-[45px] tracking-[2px] leading-13 -mt-1">
+                    FULL STACK DEVELOPER
+                </h2>
+            </div>
+
+        </div>
     </div>);
 }
 
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,20 +1,17 @@
 import type {JSX} from "react";
-import Avatar from "./Avatar.tsx";
-import AskMeInput from "./AskMeInput.tsx";
 import IntroBar from "./IntroBar.tsx";
-import {NAME} from "../constants/constants.ts";
 
 function LandingPage(): JSX.Element {
     return (
-        <div className="relative min-h-screen w-full flex flex-col pt-30 items-center">
+        <div className="relative min-h-lvh w-full h-full flex flex-col pt-30 items-center">
             <IntroBar/>
-            <Avatar/>
-            <AskMeInput/>
+            {/*<Avatar/>*/}
+            {/*<AskMeInput/>*/}
 
-            <div
-                className="hidden absolute bottom-0 bg-linear-to-b from-neutral-500/10 to-neutral-500/0 bg-clip-text text-[10rem] leading-none font-black text-transparent select-none sm:block lg:text-[16rem]">
-                {NAME}
-            </div>
+            {/*<div*/}
+            {/*    className="hidden absolute bottom-0 bg-linear-to-b from-neutral-500/10 to-neutral-500/0 bg-clip-text text-[10rem] leading-none font-black text-transparent select-none sm:block lg:text-[16rem]">*/}
+            {/*    {NAME}*/}
+            {/*</div>*/}
         </div>
     );
 
